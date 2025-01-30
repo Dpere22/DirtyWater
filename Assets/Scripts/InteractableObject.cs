c4c8b3d3fe7f9d789072c49e95b2bdc7ffd4720d
CambotH <camrenhoward@icloud.com>
2025-01-30 10:11:04 -0700

    Refactor+More Trash Functionality

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Interactable : MonoBehaviour
@@ -9,15 +10,27 @@ public class Interactable : MonoBehaviour
     public int ValueOfMaterial = 1;
 
     public bool canCollect = false;
+    public bool isQuestItem = false;
 
+    public void Start()
+    {
+        if (!isQuestItem)
+        {
+            randomGenerate();
+        }
 
+    }
 
+    private void randomGenerate()
+    {
+        Debug.Log("new trash");
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Player")) return;
 
-        Debug.Log("wowowowow");
+        Debug.Log(this.Name);
         Destroy(gameObject);
     }
 }
