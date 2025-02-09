4fe58292f2a597ccf3a3b418aaa1c5e0743411fc
CambotH <camrenhoward@icloud.com>
2025-02-09 10:33:59 -0700

    New files forgot to push

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -1,7 +1,17 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Interactable : MonoBehaviour
 {
+
+    public Dictionary<string, List<string>> setTrashData = new()
+    {
+        {"TrashBag", new List<string>
+        { "TrashBag", "Trash", "Generic", "3", "Sam_Sprite"}}
+
+
+
+    };
 
     public string Name = "";
     public string Type = "";
@@ -9,15 +19,33 @@ public class Interactable : MonoBehaviour
     public int ValueOfMaterial = 1;
 
     public bool canCollect = false;
+    public bool randomObject = true;
 
 
+    public void Start()
+    {
+        
 
+        if (randomObject)
+        {
+            randommaterial();
+        }
+    }
+
+
+    private void randommaterial()
+    {
+
+        ValueOfMaterial = int.Parse(setTrashData["TrashBag"][3]);
+
+
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Player")) return;
 
-        Debug.Log("wowowowow");
+        Debug.Log(ValueOfMaterial);
         Destroy(gameObject);
     }
 }
