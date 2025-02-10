56511c9630bead73a30c578276c72c3481cc494d
Diego Perez <u1301598@utah.edu>
2025-02-09 19:36:41 -0700

    Added new font

---
+++ Assets/RecycleUI.cs
@@ -0,0 +1,15 @@
+using TMPro;
+using UnityEngine;
+
+public class RecycleUI : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private TextMeshProUGUI text;
+    void Start()
+    {
+        int metalCount = PlayerManager.inventory["Metal"];
+        int woodCount = PlayerManager.inventory["Wood"];
+        int plasticCount = PlayerManager.inventory["Plastic"];
+        text.text = $"Metal: {metalCount} \nWood: {woodCount} \nPlastic: {plasticCount}";
+    }
+}
---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+public static class PlayerManager
+{
+    public static Dictionary<String, int> inventory = new()
+    {
+        {"Metal", 0},
+        {"Plastic", 1},
+        {"Wood", 0}
+    };
+}
