fb847301126e250de7937011fb6c782794927f31
Diego Perez <u1301598@utah.edu>
2025-02-11 10:52:30 -0700

    Added Recycle Button - Needs implemented

--- Assets/Scripts/RecycleUI.cs
+++ Assets/Scripts/RecycleUI.cs
@@ -6,6 +6,8 @@ public class RecycleUI : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private GameObject continueButton;
+    [SerializeField] private GameObject recycleButton;
     void Start()
     {
         int metalCount = PlayerManager.inventory["Metal"];
@@ -14,8 +16,22 @@ public class RecycleUI : MonoBehaviour
         text.text = $"Metal: {metalCount} \nWood: {woodCount} \nPlastic: {plasticCount}";
     }
 
+    /// <summary>
+    /// Ran when the Continue button is pressed
+    /// </summary>
     public void ContinueGame()
     {
         SceneManager.LoadScene("Main");
+    }
+
+    /// <summary>
+    /// Ran when the recycle button is pressed
+    /// </summary>
+    public void Recycle()
+    {
+        Debug.Log("Recycled");
+        continueButton.SetActive(true);
+        recycleButton.SetActive(false);
+        //TODO put recycle logic here
     }
 }
