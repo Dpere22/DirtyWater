ff46efbc97b7eecd14381461ee1a0de6adf3241e
Diego Perez <u1301598@utah.edu>
2025-03-19 17:19:09 -0600

    Movement on day end

--- Assets/Scripts/Water.cs
+++ Assets/Scripts/Water.cs
@@ -1,13 +1,26 @@
 using UnityEngine;
 using System;
+using Events;
 
 public class Water : MonoBehaviour
 {
     [SerializeField] private GameManager gameManager;
+    [SerializeField] private EdgeCollider2D waterCollider;
 
     public Action PlayerEnteredWater;
+
+    private void OnEnable()
+    {
+        GameEventsManager.Instance.DayEvents.OnDayEnd += EnableTrigger;
+    }
+
+    private void OnDisable()
+    {
+        GameEventsManager.Instance.DayEvents.OnDayEnd -= EnableTrigger;
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
+        Debug.Log("Entering Water!");
         if (!other.CompareTag("Player")) return;
         
         PlayerEnteredWater?.Invoke();
@@ -15,6 +28,12 @@ public class Water : MonoBehaviour
         playerMovement.StartSwimming();
         playerMovement.isJumping = false;
         gameManager.StartDayTimer();
-        gameObject.SetActive(false);
+        waterCollider.enabled = false;
+    }
+
+    private void EnableTrigger()
+    {
+        Debug.Log("EnablingTrigger!");
+        waterCollider.enabled = true;
     }
 }
