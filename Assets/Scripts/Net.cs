e615b14dd44e252516c864a4b8999ccb66f6bf83
Diego Perez <u1301598@utah.edu>
2025-04-14 00:45:48 -0600

    Net Sound

--- Assets/Scripts/Net.cs
+++ Assets/Scripts/Net.cs
@@ -1,11 +1,40 @@
 using Events;
+using Input;
 using UnityEngine;
 
 public class Net : MonoBehaviour
 {
+    [SerializeField] private GameObject icon;
+    private bool _inRange;
+
+    private void OnEnable()
+    {
+        GameEventsManager.Instance.InputEvents.OnSubmitPressed += DropOffTrash;
+    }
+
+    private void OnDisable()
+    {
+        GameEventsManager.Instance.InputEvents.OnSubmitPressed -= DropOffTrash;
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (!other.CompareTag("Player")) return;
+        icon.SetActive(true);
+        _inRange = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+        icon.SetActive(false);
+        _inRange = false;
+    }
+
+    void DropOffTrash(InputEventContext context)
+    {
+        if (!_inRange) return;
+        GameEventsManager.Instance.SoundEvents.PlaySoundEffect("drop_off");
         GameEventsManager.Instance.PlayerManager.DroppedOffTrash();
     }
 }
