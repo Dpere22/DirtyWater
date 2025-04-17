e46b11e5dab00afc67adb40a2d0185ff28b059e5
Diego Perez <u1301598@utah.edu>
2025-04-16 21:07:10 -0600

    Main Menu done

--- Assets/MainMenuAnim.cs
+++ Assets/MainMenuAnim.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,6 +7,7 @@ public class MainMenuAnim : MonoBehaviour
     private static readonly int FadeOut = Animator.StringToHash("fadeOut");
     [SerializeField] private GameObject mainMenu;
     [SerializeField] private Animator animator;
+    [SerializeField] private AudioSource audioSource;
     private MainMenuManager _mainMenuManager;
 
     public void Start()
@@ -16,10 +18,27 @@ public class MainMenuAnim : MonoBehaviour
 
     private void HandleClicked()
     {
+        StartCoroutine(FadeOutSound(1f));
         animator.SetTrigger(FadeOut);
     }
     private void OnCompleteAnim()
     {
         SceneManager.LoadScene(1);
+    }
+
+    private IEnumerator FadeOutSound(float duration)
+    {
+        float startVolume = audioSource.volume;
+
+        float elapsed = 0f;
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            audioSource.volume = Mathf.Lerp(startVolume, 0f, elapsed / duration);
+            yield return null;
+        }
+
+        audioSource.volume = 0f;
+        audioSource.Stop();
     }
 }
