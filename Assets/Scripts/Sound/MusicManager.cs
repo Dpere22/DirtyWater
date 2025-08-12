04a400a13cf9038e65f45532f5cdcff99ebc4d64
Diego Perez <u1301598@utah.edu>
2025-08-12 15:14:36 -0600

    Updated Music Manager

--- Assets/Scripts/Sound/MusicManager.cs
+++ Assets/Scripts/Sound/MusicManager.cs
@@ -1,67 +1,87 @@
 using System.Collections;
-using UnityEngine;
 using Events;
+using UnityEngine;
 
-public class MusicTransitionManager : MonoBehaviour
+namespace Sound
 {
-    public AudioSource musicTrack1;
-    public AudioSource musicTrack1_5;
-    public AudioSource musicTrack2;
-    public AudioSource musicTrack2_5;
-    public AudioSource musicTrack3;
-
-    public float crossfadeDuration = 2f;
-
-    private void OnEnable()
+    public class MusicTransitionManager : MonoBehaviour
     {
-        GameEventsManager.Instance.QuestEvents.OnFinishQuest += HandleQuestFinished;
-    }
+        public AudioSource musicTrack1;
+        public AudioSource musicTrack1_5;
+        public AudioSource musicTrack2;
+        public AudioSource musicTrack2_5;
+        public AudioSource musicTrack3;
 
-    private void OnDisable()
-    {
-        GameEventsManager.Instance.QuestEvents.OnFinishQuest -= HandleQuestFinished;
-    }
+        public float crossfadeDuration = 2f;
 
-    private void HandleQuestFinished(string questId)
-    {
-        if (questId == "GetToolboxQuest") 
+        private float _timeElapsed;
+        private AudioSource _to;
+        private AudioSource _from;
+        private bool _fading;
+        private float _fromStartVolume;
+        private float _toStartVolume;
+
+        private void OnEnable()
         {
-            StartCoroutine(PlaySequence(musicTrack1, musicTrack1_5, musicTrack2));
-        }
-        else if (questId == "ChestQuest") 
-        {
-            StartCoroutine(PlaySequence(musicTrack2, musicTrack2_5, musicTrack3));
-        }
-    }
-
-    private IEnumerator PlaySequence(AudioSource from, AudioSource transition, AudioSource to)
-    {
-        yield return Crossfade(from, transition);
-        yield return new WaitUntil(() => !transition.isPlaying);
-        yield return Crossfade(transition, to);
-    }
-
-    private IEnumerator Crossfade(AudioSource from, AudioSource to)
-    {
-        to.Play();
-        float timeElapsed = 0f;
-        float fromStartVolume = from.volume;
-        float toStartVolume = to.volume;
-        to.volume = 0f;
-
-        while (timeElapsed < crossfadeDuration)
-        {
-            timeElapsed += Time.deltaTime;
-            float t = timeElapsed / crossfadeDuration;
-
-            from.volume = Mathf.Lerp(fromStartVolume, 0f, t);
-            to.volume = Mathf.Lerp(0f, toStartVolume, t);
-
-            yield return null;
+            GameEventsManager.Instance.QuestEvents.OnFinishQuest += HandleQuestFinished;
         }
 
-        from.Stop();
-        from.volume = fromStartVolume;
-        to.volume = toStartVolume;
+        private void OnDisable()
+        {
+            GameEventsManager.Instance.QuestEvents.OnFinishQuest -= HandleQuestFinished;
+        }
+
+        private void Update()
+        {
+            if (!_fading) return;
+
+            if (_timeElapsed < crossfadeDuration)
+            {
+                _timeElapsed += Time.deltaTime;
+                float t = _timeElapsed / crossfadeDuration;
+
+                _from.volume = Mathf.Lerp(_fromStartVolume, 0f, t);
+                _to.volume = Mathf.Lerp(0f, _toStartVolume, t);
+            }
+            else
+            {
+                _from.Stop();
+                _fading = false;
+                _timeElapsed = 0f;
+            }
+        }
+
+        private void HandleQuestFinished(string questId)
+        {
+            if (questId == "GetToolboxQuest")
+            {
+                StartCoroutine(PlaySequence(musicTrack1, musicTrack1_5, musicTrack2));
+            }
+            else if (questId == "ChestQuest")
+            {
+                StartCoroutine(PlaySequence(musicTrack2, musicTrack2_5, musicTrack3));
+            }
+        }
+
+        private IEnumerator PlaySequence(AudioSource from, AudioSource transition, AudioSource to)
+        {
+            from.Stop();
+            transition.Play();
+            
+            float fadeStartTime = transition.clip.length - crossfadeDuration;
+            while (transition.time < fadeStartTime)
+                yield return null;
+            
+            _from = transition;
+            _to = to;
+            _fromStartVolume = _from.volume;
+            _toStartVolume = _to.volume;
+            _to.volume = 0f;
+            _to.Play();
+            _fading = true;
+            
+            while (_fading)
+                yield return null;
+        }
     }
 }
